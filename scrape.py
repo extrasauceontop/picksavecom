a6787c5da27b829f18ad8bff72004b900b6c9340
extrasauceontop <msuchemba@gmail.com>
2021-02-12 15:25:09 -0500

    cool

--- scrape.py
+++ scrape.py
@@ -33,7 +33,6 @@ session = SgRequests()
 
 x = 0
 for code in search:
-    print(x)
     url = "https://www.picknsave.com/stores/search?searchText=" + code
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
 
@@ -42,7 +41,7 @@ for code in search:
     response = session.get(url, headers=headers).text
 
     soup = bs(response, "html.parser")
-
+    print(soup)
     data = json.loads(soup.find_all("script")[-3].text.strip().split("parse(")[1].split("\')")[0][1:].replace("\\", "\\\\").replace("\\\\\\\\\"", ""))
     coords = []
     try:
